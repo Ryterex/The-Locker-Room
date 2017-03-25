e945541137c053e2d5f51dbd38d6906881da3a3b
mgmike <meng1@stevens.edu>
2017-03-25 19:56:20 -0400

    made button work again

--- ButtonClick.js
+++ ButtonClick.js
@@ -1,11 +1,10 @@
 var submitBtn = document.getElementById("submitBtn");
 
-function onSubmit{
+function onSubmit(){
 	window.alert("works");
 	var firebaseRef = firebase.database().ref("users");
 	firebaseRef.child("Mike").set({
 		email: "meng1@stevens.edu",
 		gender: "Attack Helicopter"
-	})
-	firebaseRef.child("Mike").child("Gender").setValue("Attack Helicopter");
+	});
 }
