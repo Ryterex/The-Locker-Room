6ac787bf410e6c5d887e9df6bc3c2147500734b8
mgmike <meng1@stevens.edu>
2017-03-25 17:57:18 -0400

    initial code commit

---
+++ ButtonClick.js
@@ -0,0 +1,8 @@
+var submitBtn = document.getElementById("submitBtn");
+
+function onSubmit{
+	window.alert("works");
+	//var firebaseRef = firebase.database().ref("Users");
+	//firebaseRef.child("Mike").child("Email").setValue("meng1@stevens.edu");
+	//firebaseRef.child("Mike").child("Gender").setValue("Attack Helicopter");
+}
