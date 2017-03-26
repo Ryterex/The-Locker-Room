5cdb562c854bf70a637af4fb334a8416a3ee148f
mgmike <meng1@stevens.edu>
2017-03-26 05:58:47 -0400

    register works now

--- ButtonListeners.js
+++ ButtonListeners.js
@@ -4,6 +4,18 @@ var logInButton = document.getElementById("logInButton");
 //var defaultStorage = defaultApp.storage();
 //var defaultDatabase = defaultApp.database();
 
+function onLogInRegister(){
+		window.location.href = "signin_lock.html";
+}
+
+function onLogOut(){
+	firebase.auth().signOut().then(function(){
+		window.alert("Logged out");
+	}, function(error){
+		window.alert(error);
+	});
+}
+
 
 function onSubmitRegister(){
 
@@ -16,24 +28,37 @@ function onSubmitRegister(){
 	var varifyPass = varifyPasswordText.value;
 
 	console.log("stuff");
-	/*
+	
 	if(pass === varifyPass && pass.length > 6){
 
-			console.log(email);
-			console.log(pass);
-			//window.alert("works");
-			//window.location.replace("file:///C:/Users/Mike/Documents/School/Hackathon/The-Locker-Room/profile.html");
-			*/
-			firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-			  // Handle Errors here.
-			  var errorCode = error.code;
-			  var errorMessage = error.message;
-			  console.log(errorCode);
-			  console.log(errorMessage)
-			  // ...
-			});
+		console.log(email);
+		console.log(pass);
+		//window.alert("works");
+		//window.location.replace("file:///C:/Users/Mike/Documents/School/Hackathon/The-Locker-Room/profile.html");
+		
+		firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
+		  // Handle Errors here.
+		  var errorCode = error.code;
+		  var errorMessage = error.message;
+		  console.log(errorCode);
+		  console.log(errorMessage);
+		  if(errorCode){
+		 	 window.alert(errorMessage);
+		  }
+		  
+		  // ...
+		});
+		window.location.href = "profile.html";
+	}
 
-	//}
+	if(pass != varifyPass){
+		window.alert("Passwords don't match!");
+	} else if(pass.length < 7){
+		window.alert("Password is too short!");
+	}
 	return false;
 
 }
+
+
+function on
--- CheckAuthState.js
+++ CheckAuthState.js
@@ -1,10 +1,37 @@
 function CheckAuthState(){
+
+	var logOutBtn = document.getElementById("logOutButton");
+	var logInRegisterBtn = document.getElementById("logInRegisterButton");	
+
+
+/*
 	firebase.auth().onAuthStateChanged(firebaseUser => {
+		console.log(firebaseUser);
 		if(firebaseUser){
-			var logOutBtn = document.findElementById("logOutButton");
-			var logInBtn = document.findElementById("logInButton");
 			logInBtn.style.visibility("gone");
 			logOutBtn.style.visibility("visible");
+			window.alert("You are logged in");
 		}
-	})
+		else{
+			logInBtn.style.visibility("visible");
+			logOutBtn.style.visibility("gone");
+			window.alert("You are NOT logged in");
+		}
+	});
+	*/
+
+	var user = firebase.auth().currentUser;
+		if(user){
+			window.alert("You are already logged in!");
+			//logInBtn.style.visibility("gone");
+			//logOutBtn.style.visibility("visible");
+			
+		}
+		else{
+			window.alert("Try logging in instead.");
+			//logInBtn.style.visibility("visible");
+			//logOutBtn.style.visibility("gone");
+		}
+
 }
+
